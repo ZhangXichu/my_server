e8d67661dc6eb3de9d3da99e05030f782e79dbd9
ZhangXichu <xichu.zhang@yahoo.com>
2025-03-09 15:47:28 +0100

    add Llist

---
+++ src/llist.cpp
@@ -0,0 +1,105 @@
+#include <new> 
+
+#include "llist.hpp"
+
+namespace simple_web_server {
+
+Llist::Llist()
+    :_head(nullptr),
+     _count(0) {}
+
+Llist::~Llist()
+{
+    llist_node* current = _head;
+    while (current)
+    {
+        llist_node* next = current->next;
+        delete current;
+        current->next;
+    }
+}
+
+void *Llist::insert(void *data)
+{
+    llist_node* n = new (std::nothrow) llist_node;
+    if (!n) {
+        return nullptr;
+    }
+    n->data = data;
+	n->next = _head;
+	_head = n;
+    _count++;
+
+    return data;
+}
+
+void *Llist::append(void *data)
+{
+    struct llist_node *tail = _head;
+
+    // If list is empty, just insert
+	if (tail == nullptr) {
+		return insert(data);
+	}
+
+    llist_node* n = new (std::nothrow) llist_node;
+    if (!n) {
+		return nullptr;
+	}
+
+    while (tail->next != nullptr) {
+		tail = tail->next;
+	}
+
+    n->data = data;
+    tail->next = n;
+    _count++;
+
+    return data;
+}
+
+void *Llist::head()
+{
+    if (!_head)
+    {
+        return nullptr;
+    }
+    return _head->data;
+}
+
+void *Llist::tail()
+{
+    struct llist_node *tail = _head;
+    if (!tail) {
+		return nullptr;
+	}
+
+    while (tail->next != nullptr) {
+		tail = tail->next;
+	}
+
+    return tail->data;
+}
+
+int Llist::count()
+{
+    return _count;
+}
+
+void **Llist::array_get() const
+{
+    if (!_head)
+    {
+        return nullptr;
+    }
+    // Allocate an array with an extra element for the null terminator.
+    void** a = new void*[_count + 1];
+    int i = 0;
+    for (llist_node* n = _head; n; n = n->next) {
+        a[i++] = n->data;
+    }
+    a[i] = nullptr;
+    return a;
+}
+
+}
---
+++ src/llist.hpp
@@ -0,0 +1,125 @@
+#ifndef LIST_H
+#define LIST_H
+
+namespace simple_web_server {
+
+struct llist_node {
+	void *data;
+	struct llist_node *next;
+};
+
+class Llist {
+
+public:
+
+Llist();
+~Llist();
+
+/**
+ * Insert at the head of a linked list
+ */
+void *insert(void *data);
+/**
+ * Append to the end of a list
+ */
+void *append(void *data);
+/**
+ * Return the last element in a list
+ */
+void *head();
+/**
+ * Return the last element in a list
+ */
+void *tail();
+/**
+ * Return the number of elements in the list
+ */
+int count();
+/**
+ * Allocates and returns a new NULL-terminated array of pointers to data
+ * elements in the list.
+ *
+ * NOTE: This is a read-only array! Consider it an array view onto the linked
+ * list.
+ */
+void **array_get() const;
+/**
+ * For each item in the list run a function
+ */
+template<typename F>
+void foreach(F f, void *arg)
+{
+    llist_node* p = _head;
+    llist_node* next;
+
+    while(p)
+    {
+        next = p->next;
+        f(p->data, arg);
+        p = next;
+    }
+}
+/**
+ * Find an element in the list
+ *
+ * cmpfn should return 0 if the comparison to this node's data is equal.
+ */
+template<typename Compare>
+void *find(void *data, Compare cmp)
+{
+    for (llist_node* n = head; n != nullptr; n = n->next) {
+        if (cmp(data, n->data) == 0) {
+            return n->data;
+        }
+    }
+    return nullptr;
+}
+
+/**
+ * Delete an element in the list
+ *
+ * cmpfn should return 0 if the comparison to this node's data is equal.
+ *
+ * NOTE: does *not* free the data--it merely returns a pointer to it
+ */
+template<typename Compare>
+void *l_delete(void *data, Compare cmp)
+{
+    struct llist_node *current = _head;
+    struct llist_node *prev = nullptr;
+
+    while (current)
+    {
+        if (cmp(data, current->data) == 0)
+        {
+            void* found_data = current->data;
+            if (!prev) //head
+            { 
+                _head = current->next;
+            } else { // non-head
+                prev->next = current->next;
+            }
+            delete current;
+            _count--;
+            return found_data;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    return nullptr;
+}
+
+
+
+private:
+
+llist_node* _head;
+int _count;
+
+};
+
+}
+
+
+#endif
