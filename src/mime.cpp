2c132e567f3b182ec13067151f4d5a7d2bcf2f2e
ZhangXichu <xichu.zhang@yahoo.com>
2025-04-30 08:45:11 +0200

    mime type

---
+++ src/mime.cpp
@@ -0,0 +1,41 @@
+#include "mime.hpp"
+
+#include <algorithm>
+#include <cctype>
+#include <unordered_map>
+
+namespace simple_web_server {
+
+static constexpr char DEFAULT_MIME_TYPE[] = "application/octet-stream";
+
+static const std::unordered_map<std::string, std::string> mime_map = {
+    {"html", "text/html"},
+    {"htm",  "text/html"},
+    {"jpeg", "image/jpg"},
+    {"jpg",  "image/jpg"},
+    {"css",  "text/css"},
+    {"js",   "application/javascript"},
+    {"json", "application/json"},
+    {"txt",  "text/plain"},
+    {"gif",  "image/gif"},
+    {"png",  "image/png"}
+};
+
+std::string mime_type_get(const std::string &filename) {
+    auto pos = filename.rfind('.');
+    if (pos == std::string::npos) {
+        return DEFAULT_MIME_TYPE;
+    }
+
+    std::string ext = filename.substr(pos + 1);
+    std::transform(ext.begin(), ext.end(), ext.begin(),
+                   [](unsigned char c){ return static_cast<char>(std::tolower(c)); });
+
+    auto it = mime_map.find(ext);
+    if (it != mime_map.end()) {
+        return it->second;
+    }
+    return DEFAULT_MIME_TYPE;
+}
+
+} // namespace simple_web_server
---
+++ src/mime.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+namespace simple_web_server {
+
+/**
+ * Return a MIME type for a given filename.
+ * If the extension is unknown or missing, returns "application/octet-stream".
+ */
+std::string mime_type_get(const std::string &filename);
+
+} // namespace simple_web_server
