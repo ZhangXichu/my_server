4308467d4215887661b6b03c8c7edbf74f3467dc
ZhangXichu <xichu.zhang@yahoo.com>
2025-06-01 14:15:35 +0200

    cli argument

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,6 @@
 #include "server_ws_http.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
     constexpr int  PORT          = 3496;
     unsigned int   hc            = std::thread::hardware_concurrency();
@@ -10,8 +10,13 @@ int main()
 
     std::cout << "Hardware reports " << hc << " hardware threads\n";
 
-    // TODO: make this input argument
-    auto v = my_server::load_config<my_server::ServerConfig>("/home/xichuz/workspace/my_server/configs/my_server.conf");
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <path/to/my_server.conf>\n";
+        return 1;
+    }
+
+    const std::string config_path = argv[1];
+    auto v = my_server::load_config<my_server::ServerConfig>(config_path);
 
     my_server::run_ws_http_server(
         PORT, hc, cache_size, hash_buckets, ttl_seconds, v[0].filepath_root, v[0].proxy_config_path
